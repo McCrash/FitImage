b996df483d08398fb16c3857c9e4facbee34e5bb
Ernesto Ramiro <atomms@gmail.com>
2017-11-13 20:37:55 +0100

    initial

---
+++ FitImage/src/application/Main.java
@@ -0,0 +1,78 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public AnchorPane pane;
+	public Scene scene;
+
+	// @Override
+	// public void start(Stage primaryStage) {
+	// try {
+	// BorderPane root = new BorderPane();
+	// Scene scene = new Scene(root,400,400);
+	// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	// primaryStage.setScene(scene);
+	// primaryStage.show();
+	// } catch(Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
+	//
+
+	// }
+	@Override
+	public void start(Stage stage) throws Exception {
+		// AnchorPane pane = new AnchorPane();
+		// ImageView img = new
+		// ImageView("http://weknowyourdreams.com/images/forest/forest-13.jpg");
+		// pane.setCenter(img);
+
+		/**
+		 * carga la vista fxml y opcionalmente un controlador para inicializar
+		 * algún componente sobre el panel mypane colocamos la vista
+		 */
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Root.fxml"));
+
+		loader.setController(new SetSize());
+		pane = (AnchorPane) loader.load();
+
+		scene = new Scene(pane);
+		// refugees.fitWidthProperty().bind(refugees.getScene().getWindow().widthProperty());
+		// refugees.setPreserveRatio(true);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	class SetSize {
+
+		@FXML
+		public ImageView refugees;
+
+		@FXML
+		public void initialize() {
+
+			Stage thisStage = (Stage) refugees.getScene().getWindow();
+
+			refugees.fitWidthProperty().bind(thisStage.widthProperty());
+			refugees.setPreserveRatio(true);
+
+			refugees.setEffect(new GaussianBlur(5));
+
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
